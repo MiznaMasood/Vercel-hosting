a747138f054c432b08659905e6b3cf449a8bd328
Muhammad <abdullahbinaslam64@gmail.com>
2024-07-27 20:13:42 +0500

    vercel hosting

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>My Project</h1>
+
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn_Qe8YTYISD_VB4ClVTN5ya7PXafcuRi11Q&s" alt="" />
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi dolore aut fuga omnis nam explicabo ipsum esse obcaecati alias numquam quaerat consequatur culpa, amet tenetur. Maxime pariatur quaerat molestias.</p>
+
+
+
+    </>
+  )
+}
+
+export default App
